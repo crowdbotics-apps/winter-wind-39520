405fc31b9e3d78ca96a8af0544f8a75a540f9f24
Crowdbotics <team@crowdbotics.com>
2023-03-23 14:57:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-wind-39520",
+  url: "https://winter-wind-39520.botics.co",
+  api: "https://winter-wind-39520.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
